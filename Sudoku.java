dd7bbab28d1493b521abc1a505d95acfc6de6e95
Aarti Mattoo <116307514+atumat@users.noreply.github.com>
2023-01-09 16:32:24 +0530

    Update Sudoku.java

--- Sudoku.java
+++ Sudoku.java
@@ -2,4 +2,16 @@ import java.util.Scanner;
 public class Sudoku{
   public static void main(String[] args){
     int[][]grid=readAPuzzle();
+    if(!isValid(grid)){
+      System.out.println("Invalid input");
+    }else if(search(grid)){
+      System.out.println("The solution is found");
+      printGrid(grid);
+    }else{
+      System.out.println("No solution");
+    }}
+    
+    //Reading a puzzle from input
+    
+      
     
