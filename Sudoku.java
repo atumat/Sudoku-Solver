4163aaa15468edffc7d004b54995fdde28f9dbcc
Aarti Mattoo <116307514+atumat@users.noreply.github.com>
2023-01-14 23:59:11 +0530

    Update Sudoku.java

--- Sudoku.java
+++ Sudoku.java
@@ -7,6 +7,7 @@ public class Sudoku{
     if(!isValid(grid)){
       System.out.println("Invalid input");
     }
+    
     else if(search(grid)){
       System.out.println("The solution is found");
       printGrid(grid);
